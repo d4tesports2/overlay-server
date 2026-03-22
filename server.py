39e7ddaa9348d42079d825780e479bfc4c6762bd
d4tesports2 <d4tesports2@gmail.com>
2026-03-23 01:26:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 from flask import Flask, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def home():
+    return "Server running ✅"
+
 @app.route("/live")
 def live():
-    with open("live.json", "r") as f:
-        data = json.load(f)
-    return jsonify(data)
-
-app.run(host="0.0.0.0", port=10000)
+    try:
+        file_path = os.path.join(BASE_DIR, "live.json")
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return jsonify(data)
+    except:
+        return jsonify({"error": "live.json not found"})
